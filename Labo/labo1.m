dc26fa81532169cf0a679a3627e99c3ff8650075
Philippe Spino <spip2401@usherbrooke.ca>
2017-07-19 15:08:39 -0400

    Labo1 fini

--- Labo/labo1.m
+++ Labo/labo1.m
@@ -45,35 +45,57 @@ r2 = h .* triangle;
 r3 = h .* hann;
 r4 = h .* ks;
 
-figure
-subplot(2,2,1)
-plot(abs(fft(r1)),'b');
-subplot(2,2,2)
-plot(abs(fft(r2)),'r');
-subplot(2,2,3)
-plot(abs(fft(r3)),'g');
-subplot(2,2,4)
-plot(abs(fft(r4)),'c');
+% figure
+% hold on
+% subplot(2,2,1)
+% plot(abs(fft(r1)),'b');
+% subplot(2,2,2)
+% plot(abs(fft(r2)),'r');
+% subplot(2,2,3)
+% plot(abs(fft(r3)),'g');
+% subplot(2,2,4)
+% plot(abs(fft(r4)),'c');
 
 %% Delai de groupe
 
 b1 = r1(985:1017);
 a1 = 1;
 
-b2 = r2(985:1017);
-a2 = 1;
-
-b3 = r3(985:1017);
-a3 = 1;
+% b2 = r2(985:1017);
+% a2 = 1;
+% 
+% b3 = r3(985:1017);
+% a3 = 1;
 
 b4 = r4(985:1017);
 a4 = 1;
 
+% grpdelay(b1,a1);
+% figure
+% grpdelay(b2,a2);
+% figure
+% grpdelay(b3,a3);
+% figure
+% grpdelay(b4,a4);
+
+impulse = [1,zeros(1,2*L-1)];
+cmp1 = abs(fft(filter_Qmn(impulse,b4,a4,2,7,0)));
+cmp2 = abs(fft(filter_Qmn(impulse,b4,a4,2,6,1)));
+cmp3 = abs(fft(filter_Qmn(impulse,b4,a4,2,5,2)));
+cmp4 = abs(fft(filter_Qmn(impulse,b4,a4,2,4,3)));
+cmp5 = abs(fft(filter_Qmn(impulse,b4,a4,2,3,4)));
+cmp6 = abs(fft(filter_Qmn(impulse,b4,a4,2,2,5)));
+cmp7 = abs(fft(filter_Qmn(impulse,b4,a4,2,1,6)));
+cmp8 = abs(fft(filter_Qmn(impulse,b4,a4,2,0,7)));
+
 figure
-grpdelay(b1,a1);
-figure
-grpdelay(b2,a2);
-figure
-grpdelay(b3,a3);
-figure
-grpdelay(b4,a4);
+hold on
+% plot(cmp1);
+% plot(cmp2);
+% plot(cmp3);
+% plot(cmp4);
+% plot(cmp5);
+% plot(cmp6);
+% plot(cmp7);
+plot(cmp8);
+plot(abs(fft(r4)),'b');
