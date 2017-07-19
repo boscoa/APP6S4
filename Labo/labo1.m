566c7c771b94f81a6a92c1235d21db43e353289d
Philippe Spino <spip2401@usherbrooke.ca>
2017-07-19 14:51:19 -0400

    fix labo 1 Delai de groupe

--- Labo/labo1.m
+++ Labo/labo1.m
@@ -8,9 +8,10 @@ L = 1000;
 
 N = 31;
 alpha = 2.65;
-H = [ones(1,275) zeros(1,485) ones(1,240)];
+% H = [ones(1,13) zeros(1,25) ones(1,12)];
 % H = [ones(1,27) zeros(1,49) ones(1,24)];
-H = [H,H];
+H = [ones(1,275) zeros(1,485) ones(1,240)];
+H = [H,H(end), H(end:-1:2)];      % Le mettre sur 2PI pts
 h = ifftshift(ifft(H));
 
 % figure
@@ -24,33 +25,20 @@ h = ifftshift(ifft(H));
 % plot(abs(ifftshift(ifft(H))));
 % ylim([0 1.1])
 
-carree = [zeros(1,985) ones(1,31) zeros(1,984)];
+carree = [zeros(1,985) ones(1,30) zeros(1,985)];
 triangle = [zeros(1,985) triang(31)' zeros(1,984)];
 hann = [zeros(1,985) hanning(31)' zeros(1,984)];
 ks = [zeros(1,985) kaiser(31,alpha)' zeros(1,984)];
 
-figure,
-hold on
-plot(carree,'b')
-plot(triangle,'r')
-plot(hann,'g')
-plot(ks,'c')
-hold off 
-xlim([0 2*L+1])
-ylim([0 1.2])
-
-% Carree = abs(fftshift(fft(carree)));
-% Triangle = abs(fftshift(fft(triangle)));
-% Hann = abs(fftshift(fft(hann)));
-% Ks = abs(fftshift(fft(ks)));
-
-% figure
+% figure,
 % hold on
-% plot(Carree,'b');
-% plot(Triangle,'r');
-% plot(Hann,'g');
-% plot(Ks,'c');
+% plot(carree,'b')
+% plot(triangle,'r')
+% plot(hann,'g')
+% plot(ks,'c')
 % hold off 
+% xlim([0 2*L+1])
+% ylim([0 1.2])
 
 r1 = h .* carree;
 r2 = h .* triangle;
@@ -66,3 +54,26 @@ subplot(2,2,3)
 plot(abs(fft(r3)),'g');
 subplot(2,2,4)
 plot(abs(fft(r4)),'c');
+
+%% Delai de groupe
+
+b1 = r1(985:1017);
+a1 = 1;
+
+b2 = r2(985:1017);
+a2 = 1;
+
+b3 = r3(985:1017);
+a3 = 1;
+
+b4 = r4(985:1017);
+a4 = 1;
+
+figure
+grpdelay(b1,a1);
+figure
+grpdelay(b2,a2);
+figure
+grpdelay(b3,a3);
+figure
+grpdelay(b4,a4);
