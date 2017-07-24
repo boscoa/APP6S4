c6c7c89c0f79b050c50cd3858d643754547c6841
Philippe Spino <spip2401@usherbrooke.ca>
2017-07-24 10:15:23 -0400

    ajout du RIF, RIFsetting.fda et RIFsetup(function)

--- RIF/RIF.m
+++ RIF/RIF.m
@@ -5,5 +5,7 @@ clc
 [s1, Fe] = audioread('Sons/signal_A.wav');
 [s2, Fe] = audioread('Sons/signal_B.wav');
 
-ffts1 = abs(fft(s1,Fe));
-plot(1:Fe/2,ffts1(1:Fe/2));
+RIFlowpass = RIFsetup();
+
+r = filter(RIFlowpass,s1);
+r = downsample(r,6);
